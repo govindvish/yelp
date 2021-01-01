6420311e6ddbde835617ab614ddc7c031b82d1f0
Govind Vishwakarma <vishwakarmagovind22@gmail.com>
2021-01-01 22:13:52 +0530

    Setup concurrently and React Routes.

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,29 @@
+import {
+  BrowserRouter as Router,
+  Redirect,
+  Route,
+  Switch,
+} from 'react-router-dom';
 import './App.css';
+import Home from './containers/Home';
+import RestaurantDetails from './containers/RestaurantDetails';
+import Restaurants from './containers/Restaurants';
 
 function App() {
   return (
     <div>
-      <h1>Hello World!!</h1>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route
+            exact
+            path="/restaurants/:id/update"
+            component={RestaurantDetails}
+          />
+          <Route exact path="/restaurants/:id" component={Restaurants} />
+          <Redirect to="/" />
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ client/src/containers/RestaurantDetails.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const RestaurantDetails = () => {
+  return (
+    <div>
+      <h1>RestaurantDetails</h1>
+    </div>
+  );
+};
+
+export default RestaurantDetails;
